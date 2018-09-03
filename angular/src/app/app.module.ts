d50f58405cbf2223f722235b59a9b11bade10c55
Maks Koseda <maks.koseda@gmail.com>
2018-09-03 18:37:00 +0200

    Sidenav reconstruction

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatToolbarModule } from '@angular/material';
-import { MatMenuModule, MatButtonModule, MatIconModule, MatCardModule, MatSidenavModule, MatTabsModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import { 
+  MatToolbarModule, 
+  MatMenuModule, 
+  MatButtonModule, 
+  MatIconModule, 
+  MatCardModule, 
+  MatSidenavModule, 
+  MatTabsModule, 
+  MatListModule 
+} from '@angular/material';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { PlanComponent } from './plan/plan.component';
+import { SubstitutionsComponent } from './substitutions/substitutions.component';
+import { SettingsComponent } from './settings/settings.component';
+const appRoutes: Routes = [
+  { path: '', component: MainComponent, data: { title: 'Strona Główna' } },
+  { path: 'main', component: MainComponent, data: { title: 'Strona Główna' } },
+  { path: 'plan', component: PlanComponent, data: { title: 'Plan lekcji' } },
+  { path: 'substitutions', component: SubstitutionsComponent, data: { title: 'Zastępstwa' } },
+  { path: 'settings', component: SettingsComponent, data: { title: 'Ustawienia' } },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanComponent,
+    SubstitutionsComponent,
+    SettingsComponent,
+    NavComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +44,12 @@ import { AppComponent } from './app.component';
     MatIconModule,
     MatCardModule,
     MatSidenavModule,
-    MatTabsModule
+    MatTabsModule,
+    MatListModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { useHash: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
