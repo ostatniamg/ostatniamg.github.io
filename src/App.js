b866bf050a835b9491fc2c787f0158ef276473f9
Maks Koseda <maks.koseda@gmail.com>
2018-10-21 13:55:26 +0200

    Content moved to seperate component, nav moved to App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 
-import UINav from './components/UINav';
-import CardDefault from './components/CardDefault';
-import CardExpandable from './components/CardExpandable';
+import Content from './Content';
 
 import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+import Drawer from '@material-ui/core/Drawer';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+
+import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
 
 const styles = {
   content: {
@@ -15,63 +25,81 @@ const styles = {
 };
 
 class App extends Component {
-  content = {
-    Today: (
-      <CardDefault
-        title="CardDefault"
-        teacher="Teacher"
-        room="Room"
-      ></CardDefault>
-    ),
-    Tomorrow: (
-      <CardExpandable
-        title="CardExpandable"
-        content={
-          <div>
-            <CardDefault
-              title="CardDefault"
-              teacher="Teacher"
-              room="Room"
-            ></CardDefault>
-            <CardDefault
-              title="CardDefault"
-              teacher="Teacher"
-              room="Room"
-              value={50}
-            ></CardDefault>
-            <CardDefault
-              title="CardDefault"
-              teacher="Teacher"
-              room="Room"
-              inactive
-            ></CardDefault>
-          </div>
-        }
-      ></CardExpandable>
-    ),
-    In2Days: (
-      <div></div>
-    ),
+  state = {
+    open: false,
   };
 
+  toggleDrawer = (value) => () => {
+    this.setState({
+      open: value,
+    });
+  };
+  
   render() {
     const { classes } = this.props;
-
+    const sideList = (
+      <div>
+        <List>
+          <ListItem button onClick={() => { this.content.switchTo('main') }}>
+            <ListItemIcon>
+              <Icon>home</Icon>
+            </ListItemIcon>
+            <ListItemText primary="Strona głowna"/>
+          </ListItem>
+          <ListItem button onClick={() => { this.content.switchTo('plan') }}>
+            <ListItemIcon>
+              <Icon>calendar_today</Icon>
+            </ListItemIcon>
+            <ListItemText primary="Plan Lekcji"/>
+          </ListItem>
+          <ListItem button onClick={() => { this.content.switchTo('substitutions') }}>
+            <ListItemIcon>
+              <Icon>list</Icon>
+            </ListItemIcon>
+            <ListItemText primary="Zastępstwa"/>
+          </ListItem>
+          <ListItem button onClick={() => { this.content.switchTo('settings') }}>
+            <ListItemIcon>
+              <Icon>settings</Icon>
+            </ListItemIcon>
+            <ListItemText primary="Ustawienia"/>
+          </ListItem>
+        </List>
+      </div>
+    );
     return (
       <div className="App">
-        <UINav></UINav>
+        <div className={classes.nav}>
+          <AppBar position="sticky">
+            <Toolbar>
+              <IconButton color="inherit" aria-label="Menu" onClick={this.toggleDrawer(true)}>
+                <Icon>menu</Icon>
+              </IconButton>
+              <Typography variant="h6" color="inherit" className={this.props.classes.title} >
+                Strona główna
+              </Typography>
+            </Toolbar>
+          </AppBar>
+
+          <Drawer open={this.state.open} onClose={this.toggleDrawer(false)}>
+            <div
+              tabIndex={0}
+              role="button"
+              onClick={this.toggleDrawer(false)}
+              onKeyDown={this.toggleDrawer(false)}
+            >
+              {sideList}
+            </div>
+          </Drawer>
+        </div>
+
         <div className={classes.content}>
-          <Typography variant="h4" gutterBottom>Today</Typography>
-          { this.content.Today }
-          <Typography variant="h4" gutterBottom>Tomorrow</Typography>
-          { this.content.Tomorrow }
-          <Typography variant="h4" gutterBottom>In 2 days</Typography>
-          { this.content.In2Days }
+          <Content ref={instance => { this.content = instance }}></Content>
         </div>
       </div>
     );
-  }
-}
+  };
+};
 
 App.propTypes = {
   classes: PropTypes.object.isRequired,
